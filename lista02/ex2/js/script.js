775cc151046b41a7dda9de8325f1dfbd5faf7dc0
Marcelovssl <163941720+Marcelovssl@users.noreply.github.com>
2026-08-21 19:32:14 -0300

    Update script.js

--- lista02/ex2/js/script.js
+++ lista02/ex2/js/script.js
@@ -1,4 +1,4 @@
 var nome = prompt("Digite seu nome completo:");
-document.write("Nome informado: " + nome + "<br>");
-document.write("Quantidade de caracteres: " + nome.length);
+document.write("Nome informado: " + ${nome} + "<br>");
+document.write("Quantidade de caracteres: " + ${nome.length});
  
